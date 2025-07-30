be567f706fc8221524ae4daa83713e5bd1ccf6eb
14642BEN <kilonzoben007@gmail.com>
2025-07-30 12:15:23 +0300

    server running

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ mongoose.connect(process.env.MONGO_URI, {
   console.log("✅ Connected to MongoDB");
 
   // Important: use Render's injected PORT
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 10000;
   app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
   });
